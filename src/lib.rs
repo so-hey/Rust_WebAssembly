8ec9a6e5e9a730939af0e63659560cf860207692
so-hey <soh.tngt@gmail.com>
2023-11-23 19:37:10 +0900

    Drawing Red Hat Boy

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use futures::AsyncBufRead;
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 use wasm_bindgen::JsCast;
@@ -32,7 +33,22 @@ pub fn main_js() -> Result<(), JsValue> {
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
         .unwrap();
 
-    sierpinski(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)], 4);
+    wasm_bindgen_futures::spawn_local(async move {
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<()>();
+        let image = web_sys::HtmlImageElement::new().unwrap();
+
+        let callback = Closure::once(|| {
+            success_tx.send(());
+        });
+
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_src("Idle (1).png");
+
+        success_rx.await;
+        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
+
+        sierpinski(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)], 4);
+    });
 
     // JavaScript の console.log関数は可変個の引数を受け取るので, log_i とするとi個の引数をとるように指定できる.
     // ここでは1つの引数をとっている.
